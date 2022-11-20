2bb070be9fb33bc70e2688fdc4ae87da86069874
hamdiboyraz <hamdiboyraz19@gmail.com>
2022-11-20 03:12:42 +0300

    Reuse components

--- src/Home.js
+++ src/Home.js
@@ -12,6 +12,7 @@ const Home = () => {
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs"/>
+            <BlogList blogs={blogs.filter(i => i.author === "mario")} title="Mario's Blogs"/>
         </div>
      );
 }
